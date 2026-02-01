06aa1e600e5a91dc667eceb35d3218ea9eebea43
IvanKolesn <ivan_kolesnikov@yahoo.com>
2026-02-01 15:50:05 +0300

    Added conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,31 @@
+"""
+Fixtures for testing
+"""
+
 import pytest
+
+
+@pytest.fixture
+def usd_only_portfolio():
+    """
+    Portfolio with only USD
+    """
+    return {"usd": 0, "eur": 0}
+
+
+@pytest.fixture
+def historical_exchange_rate():
+    """
+    Fake numbers
+    """
+    data = {
+        "eurjpy": {
+            pd.Timestamp("2024-12-01 09:00:00"): 100.0,
+            pd.Timestamp("2024-12-02 09:00:00"): 100.0,
+        },
+        "eurusd": {
+            pd.Timestamp("2024-12-01 09:00:00"): 2.0,
+            pd.Timestamp("2024-12-02 09:00:00"): 2.0,
+        },
+    }
+    return pd.DataFrame.from_dict(data)
